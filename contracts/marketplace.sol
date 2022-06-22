8cb31452f2db88e11ac5dc11816417b3dcc45094
Axat Bhardwaj <axat@solulab.com>
2022-06-22 23:15:29 +0530

    todo pushed

---
+++ contracts/marketplace.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8;
+
+contract markteplace {
+/*
+TODO :
+
+1 create NFT with multiple owners
+2 Set Nft price
+3 auction
+4 royalty 
+5 platform fee
+
+*/
+
+
+
+}
